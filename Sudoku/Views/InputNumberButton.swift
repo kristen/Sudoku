5b70b7ee28a60d3001880aab20c66cf601b78be4
Kristen Sundquist <kssundquist@gmail.com>
2015-10-10 22:45:41 -0700

    Add number input buttons

---
+++ Sudoku/Views/InputNumberButton.swift
@@ -0,0 +1,28 @@
+//
+//  InputNumberButton.swift
+//  Sudoku
+//
+//  Created by Kristen Sundquist on 10/10/15.
+//  Copyright © 2015 Kristen Sundquist. All rights reserved.
+//
+
+import UIKit
+
+class InputNumberButton: UIButton {
+
+    let number: Int!
+    
+    init(frame: CGRect, number: Int) {
+        self.number = number
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.whiteColor()
+        self.setTitle(String(self.number), forState: UIControlState.Normal)
+        self.setTitleColor(UIColor.blackColor(), forState: .Normal)
+
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- Sudoku/Views/SudokuGameBoardView.swift
+++ Sudoku/Views/SudokuGameBoardView.swift
@@ -11,8 +11,9 @@ import UIKit
 class SudokuGameBoardView: UIView {
     
     init() {
+        super.init(frame: UIScreen.mainScreen().bounds)
+        
         let gameBoardSize = UIScreen.mainScreen().bounds.width
-        super.init(frame: CGRectMake(0, 0, gameBoardSize, gameBoardSize))
         let sectionDividerWidth: CGFloat = 5
         let remainingGameBoardSize: CGFloat = gameBoardSize - 2 * sectionDividerWidth
         let sectionSize: CGFloat = remainingGameBoardSize / 3
@@ -34,18 +35,26 @@ class SudokuGameBoardView: UIView {
         self.addSubview(sectionView3)
         self.addSubview(sectionView4)
         
-        
-        
         let spaceBetweenCells: CGFloat = 5
         let cellSize: CGFloat = (sectionSize - 2 * spaceBetweenCells) / 3
         
+        let numberButtonYPosition = gameBoardSize + (UIScreen.mainScreen().bounds.height - gameBoardSize) / 2
+        
         for row in 0...8 {
+            let xPosition = CGFloat(row) * cellSize + CGFloat(row) * spaceBetweenCells
+            
+            let numberButtonFrame = CGRectMake(xPosition, numberButtonYPosition, cellSize, cellSize)
+            let inputButton = InputNumberButton(frame: numberButtonFrame, number: row + 1)
+            
+            inputButton.addTarget(self, action: "click:", forControlEvents: UIControlEvents.TouchUpInside)
+            self.addSubview(inputButton)
+            
             for column in 0...8 {
-                let xPosition = CGFloat(row) * cellSize + CGFloat(row) * spaceBetweenCells
                 let yPosition = CGFloat(column) * cellSize + CGFloat(column) * spaceBetweenCells
                 let cellFrame = CGRectMake(xPosition, yPosition, cellSize, cellSize)
                 let sudokuCellView = SudokuCellView(frame: cellFrame)
                 self.addSubview(sudokuCellView)
+
             }
         }
         
@@ -54,4 +63,9 @@ class SudokuGameBoardView: UIView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func click(sender: InputNumberButton) {
+        print(sender.number)
+    }
+    
 }
